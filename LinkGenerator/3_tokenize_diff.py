95a149f86e8a29a644f38410b01cc166382d0ea8
Jason Lao <49947837+Jlao17@users.noreply.github.com>
2025-07-26 16:52:38 +0200

    Update 3_tokenize_diff.py

--- LinkGenerator/3_tokenize_diff.py
+++ LinkGenerator/3_tokenize_diff.py
@@ -8,35 +8,9 @@ from tqdm import tqdm
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 stemmer = nltk.stem.SnowballStemmer('english')
 repo = "HADOOP"
-# Define the repo and the specific commit hash you want
-# repo_path = "https://github.com/apache/calcite"  # Change to your repo path or  local directory
+
 df = pd.read_csv(f"../data/Processed{repo.title()}/2.5_{repo.lower()}_link_false_rn.csv")
 tqdm.pandas()
-# def codeMatch(word):
-#     identifier_pattern = r'''[A-Zz-z]+[0-9]*_.*
-#                             |[A-Za-z]+[0-9]*[\.].+
-#                             |[A-Za-z]+.*[A-Z]+.*
-#                             |[A-Z0-9]+
-#                             |_ +[A-Za-z0-9]+.+
-#                             |[a-zA-Z]+[:]{2,}.+
-#                             '''
-#     identifier_pattern = re.compile(identifier_pattern)
-#     if identifier_pattern.match(word):
-#         return True
-#     else:
-#         return False
-#
-# def diffProcess(text):
-#     if not isinstance(text, str) or pd.isna(text):
-#         return ' '  # Return empty space or other default value
-#     sentences = tokenizer.tokenize(text)
-#     for sentence in sentences:
-#         word_tokens = word_tokenize(sentence)
-#         # if a token is 2 characters or less, remove it
-#         word_tokens = [word for word in word_tokens if len(word) > 2]
-#         print(word_tokens)
-#         return word_tokens
-
 
 df['Diff_processed'] = df['Diff_processed'].progress_apply(preprocessor.processCode)
 df.to_csv(f"../data/Processed{repo.title()}/3_{repo.lower()}_link_final_diff_processed.csv", index=False)
